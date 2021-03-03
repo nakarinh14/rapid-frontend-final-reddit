06beec9be81d26a1ce6a6a56211c3386f20477e9
nakarinh14 <nakarinh14@gmail.com>
2021-03-03 23:30:05 +0700

    Add Bottom Navigation Template and Home screen component

---
+++ RapidReddit/src/screens/Home.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {Block} from "galio-framework";
+import {firebase} from "../firebase";
+import "firebase/auth";
+
+export const Home = ({navigation}) => {
+
+    return (
+        <Block>
+        </Block>
+    )
+}

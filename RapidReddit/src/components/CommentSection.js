9d178d645593550869cd608ba107e67f3a285dbe
nakarinh14 <nakarinh14@gmail.com>
2021-03-05 01:14:34 +0700

    Add Post screen and update Comment components

--- RapidReddit/src/components/CommentSection.js
+++ RapidReddit/src/components/CommentSection.js
@@ -6,117 +6,23 @@ import {CommentTree} from "./CommentTree";
 
 const { width } = Dimensions.get('screen');
 
-const _comments = {
-    "1": {
-        user: "a",
-        timestamp: "2h",
-        body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-        upvotes: 50,
-        comments: {
-            "1": {
-                user: "a",
-                timestamp: "2h",
-                body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-                upvotes: 50
-
-            }
-        }
-    },
-    "2": {
-        user: "a",
-        timestamp: "2h",
-        body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-        upvotes: 50,
-        comments: {
-            "1": {
-                user: "a",
-                timestamp: "2h",
-                body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-                upvotes: 50
-            },
-            "2":{
-                user: "YeetMeToTheMoon",
-                timestamp: "10m",
-                body: "Na u gonna yeet me",
-                upvotes: 1012,
-                comments:{
-                    "1": {
-                        user: "a",
-                        timestamp: "5m",
-                        body: "nope I won't",
-                        upvotes: 50,
-                        comments: {
-                            "1":{
-                                user: "YeetMeToTheMoon",
-                                timestamp: "1m",
-                                body: "yes u do",
-                                upvotes: 1012,
-                            }
-                        }
-                    },
-                }
-            }
-        },
-    },
-    "3": {
-        user: "a",
-        timestamp: "5d",
-        body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-        upvotes: 50,
-        comments: {
-            "1": {
-                user: "a",
-                timestamp: "1mo",
-                body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-                upvotes: 50
-            }
-        },
-    },
-    "4": {
-        user: "a",
-        timestamp: "23h",
-        body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-        upvotes: 50,
-        comments: {
-            "1": {
-                user: "a",
-                timestamp: "2h",
-                body: "[PURCHASE guide] 2020, ASK ANYTHING!",
-                upvotes: 50
-            }
-        },
-    }
-};
-
-const useMockData = true
-
-export const CommentSection = ()=> {
-
-    const [comments, setComments] = useState(_comments)
-
-    useEffect(() => {
-        if(useMockData){
-            setComments(_comments)
-        }
-    })
+export const CommentSection = ({ comments })=> {
 
     return (
-        <ScrollView>
-            <Block flex style={styles.cards}>
-                {comments && Object.keys(comments).map((comment_id) => {
-                    const comment = comments[comment_id]
-                    return (
-                        <Block
-                            key={`card-${comment_id}`}
-                            flex
-                            style={styles.card}
-                        >
-                           <CommentTree comment={comment} depth={0} />
-                        </Block>
-                    )}
+        <Block flex style={styles.cards}>
+            {comments && Object.keys(comments).map((comment_id) => {
+                const comment = comments[comment_id]
+                return (
+                    <Block
+                        key={`card-${comment_id}`}
+                        flex
+                        style={styles.card}
+                    >
+                       <CommentTree comment={comment} depth={0} />
+                    </Block>
                 )}
-            </Block>
-        </ScrollView>
+            )}
+        </Block>
     )
 }
 

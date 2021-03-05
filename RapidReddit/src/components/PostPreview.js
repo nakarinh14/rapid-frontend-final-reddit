33a227a3a5b8dd6b400e72fc1a0e8943a20f68ad
nakarinh14 <nakarinh14@gmail.com>
2021-03-05 22:28:55 +0700

    Update PostPreview UI

--- RapidReddit/src/components/PostPreview.js
+++ RapidReddit/src/components/PostPreview.js
@@ -1,74 +1,141 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import {Block, Card, Icon, Text} from 'galio-framework';
+import { TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import {Block, Text} from 'galio-framework';
 import theme from '../theme';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
-const { width, height } = Dimensions.get('screen');
+const { width } = Dimensions.get('screen');
 
-export default class PostPreview extends React.Component {
-    render() {
-        const { title, iconName, iconFamily, caption, updoots } = this.props;
-        console.log(this.props)
-        // styles.card.height = heightFixed || height * heightMultiplier || 200
-        // cardStyle.height = heightFixed || height * heightMultiplier || 200
-        return (
-            <Block card style={styles.card}>
-                <Block row style={styles.cardContent}>
-                    <Block flex={1}>
-                        <TouchableOpacity>
-                            <Block row>
-                                <Block left center flex={1} style={styles.cardIcon}>
-                                    <Icon name={iconName} family={iconFamily} size={theme.SIZES.BASE * 3}/>
-                                </Block>
-                                <Block styles={styles.contentArea} flex={3}>
-                                    <Block>
-                                        <Text size={theme.SIZES.FONT }>{title}</Text>
-                                    </Block>
-                                    <Block>
-                                        <Text size={theme.SIZES.FONT * 0.7} muted>{caption}</Text>
-                                    </Block>
-                                </Block>
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna";
+
+export const PostPreview = () => {
+
+    return (
+        <Block style={styles.card}>
+            <Block row style={styles.cardContent}>
+                <Block flex={1}>
+                    <Block row>
+                        <Block flex={3}>
+                            <Block style={styles.title}>
+                                <Text style={styles.titleText}>
+                                    {lorem}
+                                </Text>
                             </Block>
+                            <Block style={styles.content}>
+                                <Text style={styles.contentText}>
+                                    {lorem}
+                                </Text>
+                            </Block>
+                        </Block>
+                    </Block>
+                </Block>
+                <Block style={{marginVertical: 10}} row>
+
+                    <Block style={{marginRight: 5}} center row>
+                        <Text style={styles.groupText} size={14} color={theme.COLORS.BLOCK}>
+                            {'Investment'}
+                        </Text>
+                        <Text color={theme.COLORS.MUTED}> by </Text>
+                        <Text style={styles.groupText} size={14} color={theme.COLORS.BLOCK}>
+                            {'IAmNotAUser'}
+                        </Text>
+                    </Block>
+                    <Block center row>
+                        <Ionicons name="ios-time-outline" size={15} color={theme.COLORS.BLOCK} />
+                        <Text size={14} color={theme.COLORS.BLOCK}>{'2h'}</Text>
+                    </Block>
+                </Block>
+                <Block row style={styles.bottomActions}>
+                    <Block center row>
+                        <TouchableOpacity>
+                            <MaterialCommunityIcons
+                                size={24}
+                                name="arrow-up-bold-outline"
+                                color={theme.COLORS.BLOCK}
+                            />
+                        </TouchableOpacity>
+                        <Block>
+                            <Text
+                                size={15}
+                                color={theme.COLORS.BLOCK}
+                                style={{marginLeft: 3, marginRight: 3, fontWeight: '500'}}
+                            >
+                                {15}
+                            </Text>
+                        </Block>
+                        <TouchableOpacity>
+                            <MaterialCommunityIcons
+                                size={24}
+                                name="arrow-down-bold-outline"
+                                color={theme.COLORS.BLOCK}
+                            />
                         </TouchableOpacity>
                     </Block>
-                    <Block center style={styles.sideActions}>
-                        <TouchableOpacity>
-                            <Block>
-                                <Icon name="arrow-bold-up" family="Entypo" size={theme.SIZES.BASE * 1.25}/>
-                            </Block>
-                        </TouchableOpacity>
-                        <Block><Text>{updoots}</Text></Block>
-                        <TouchableOpacity>
-                            <Block><Icon name="arrow-bold-down" family="Entypo" size={theme.SIZES.BASE * 1.25}/></Block>
-                        </TouchableOpacity>
+                    <Block row center>
+                        <Block style={{marginRight: 5}} center>
+                            <MaterialCommunityIcons
+                                name="comment-outline"
+                                size={20}
+                                color={theme.COLORS.BLOCK}
+                            />
+                        </Block>
+                        <Text style={{fontWeight: '500'}} size={15} color={theme.COLORS.BLOCK}>
+                            {25}
+                        </Text>
+                    </Block>
+                    <Block row center>
+                        <Block style={{marginRight: 5}} center>
+                            <Ionicons name="share-outline" size={22} color={theme.COLORS.BLOCK}/>
+                        </Block>
+                        <Text style={{fontWeight: '500'}} size={15} color={theme.COLORS.BLOCK}>
+                            {'Share'}
+                        </Text>
+                    </Block>
+                    <Block row center>
+                        <Ionicons
+                            name="ios-ellipsis-horizontal"
+                            size={21}
+                            color={theme.COLORS.BLOCK}
+                        />
                     </Block>
                 </Block>
             </Block>
-        )
-    }
+        </Block>
+    )
+
 }
 
 const styles = StyleSheet.create({
     card: {
         width: width * 0.98,
-        margin: width * 0.01,
+        padding: 10,
         backgroundColor: theme.COLORS.WHITE
 
     },
     cardContent: {
-        padding: theme.SIZES.BASE * 0.3
+        padding: theme.SIZES.BASE * 0.3,
+        flexDirection: "column"
     },
     cardIcon: {
-        // marginRight: theme.SIZES.BASE
         padding: theme.SIZES.BASE * 0.5 ,
-        // backgroundColor: theme.COLORS.PRIMARY
     },
-    contentArea: {
-        // backgroundColor: theme.COLORS.PAPER
+    bottomActions: {
+        justifyContent: "space-between",
     },
-    sideActions: {
-        marginRight: theme.SIZES.BASE * 0.5
+    title:{
+      marginBottom: 15
+    },
+    titleText:{
+      fontSize: 17
+    },
+    contentText: {
+        color: theme.COLORS.BLOCK
+    },
+    groupText: {
+        fontWeight: '500',
     }
+
 })
 
 

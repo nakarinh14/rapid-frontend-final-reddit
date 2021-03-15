01478ddd9e1a54f9f25151588ee39ba884f7a0fa
williampariwat <william.pariwat@gmail.com>
2021-03-16 00:34:33 +0700

    Adding registration page and also fix some bugs

--- web-end/src/layouts/MainLayout.vue
+++ web-end/src/layouts/MainLayout.vue
@@ -1,14 +1,17 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-
-    <q-page-container>
       <q-page class="bg-orange-1 window-height window-width row justify-center items-center">
         <div class="column">
-          <div class="row ">
-            <h5 class="text-h5 text-orange-4 q-my-md">Login</h5>
-          </div>
+          <q-card-section class="bg-orange-8" style="height: 80px">
+            <q-img class="label"
+                   src="https://www.vectorico.com/download/social_media/Reddit-Icon.png"
+                   spinner-color="white"
+                   style="max-height: 100%; max-width:15%; left: 130px ;"
+            />
+          </q-card-section>
+
           <div class="row">
-            <q-card square bordered class="q-pa-lg shadow-1">
+            <q-card square bordered style="width: 350px" class="q-pa-lg shadow-1">
               <q-card-section>
                 <q-form class="q-gutter-md">
                   <q-input square filled clearable v-model="email" type="email" label="Email"/>
@@ -21,7 +24,6 @@
           </div>
         </div>
       </q-page>
-    </q-page-container>
   </q-layout>
 </template>
 

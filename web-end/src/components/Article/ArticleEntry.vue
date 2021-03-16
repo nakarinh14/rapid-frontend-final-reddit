7ecdbc295aaf0453132972265d71aefd77549672
williampariwat <william.pariwat@gmail.com>
2021-03-16 21:41:41 +0700

    Adding article entry file

---
+++ web-end/src/components/Article/ArticleEntry.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-item class="column" clickable>
+    <div class="row">
+
+      <!-- Article Title and info -->
+      <q-item-section class="col">
+        <q-item-label lines="1">
+          <span class="text-h6">{{article.title}}</span>
+        </q-item-label>
+
+        <q-item-label lines="1" class="text-grey">
+          <router-link @click.native.stop :to="'/r/' + article.subreddit">
+            r/{{article.subreddit}}
+          </router-link>
+          <span>．Posted by </span>
+          <router-link @click.native.stop :to="'/u/' + article.postedBy">
+            u/{{article.postedBy}}
+          </router-link>
+          <span> on {{article.postedTime | formatDate}}</span>
+        </q-item-label>
+      </q-item-section>
+    </div>
+  </q-item>
+</template>
+
+<script>
+export default {
+name: "ArticleEntry.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- web-end/src/layouts/MainLayout.vue
+++ web-end/src/layouts/MainLayout.vue
@@ -1,56 +1,53 @@
 <template>
+  <q-toolbar class="bg-orange-8 text-white">
+    <q-avatar @click="linkToHome" class="cursor-pointer">
+      <img src="https://www.vectorico.com/download/social_media/Reddit-Icon.png" />
+    </q-avatar>
 
+    <q-toolbar-title @click="linkToHome" class="cursor-pointer">
+      Reddit
+    </q-toolbar-title>
 
+    <q-input @keydown.enter.prevent="submitSearch" class="q-ml-xl" style="width: 400px" dark clearable outlined dense standout v-model="searchText" label="Search" :maxlength="searchTermMaxLen" />
+    <q-space />
+  </q-toolbar>
+  <div class="q-pa-md text-white">
+    <q-list dark bordered separator class="bg-grey-10 rounded-borders">
+      <q-infinite-scroll @load="loadMoreArticles" :offset="250" ref="infiniteScroll">
+        <article-entry
+          v-for="article in articles"
+          :key="article.articleID"
+          :article="article"
+          @click.native="articleClicked(article)"
+        />
+      </q-infinite-scroll>
+
+      <!-- Error Indicator -->
+      <q-banner v-if="errOccurred" class="text-white bg-red">
+        Error fetching the article list, please try again.
+      </q-banner>
+
+      <!-- Empty Indicator -->
+      <q-item v-if="!loading && articles.length == 0 && !errOccurred" class="column">
+        <div class="q-ma-lg">
+          <div class="text-h3 q-mb-md">Wow, such empty!</div>
+          <div class="text-h6">{{emptyText}}</div>
+        </div>
+      </q-item>
+
+    </q-list>
+  </div>
 </template>
 
 <script>
 // import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
+
+const articles = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    titl
   }
 ]
-
 export default {
   name: 'MainLayout',
   // components: { EssentialLink },

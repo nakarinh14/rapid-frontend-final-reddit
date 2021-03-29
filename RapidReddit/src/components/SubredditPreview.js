bc8a963697de4b9702cb98cb75d5417deb28b889
nakarinh14 <nakarinh14@gmail.com>
2021-03-29 11:20:30 +0700

    Update Subreddit Preview

--- RapidReddit/src/components/SubredditPreview.js
+++ RapidReddit/src/components/SubredditPreview.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Card } from 'galio-framework';
+import theme from "../theme";
 
 function SubredditPreview({subreadit}){
     return (
         <Card
+            shadow={false}
             style={styles.subredditPreview}
             title={subreadit.name}
             caption={subreadit.description}
-            avatar="https://i.imgur.com/JcUJXxgl.png">
+            avatar="https://globalactionplan.ie/wp-content/uploads/2016/08/Community-Icon.jpg">
         </Card>
     )
 }
 
 const styles = StyleSheet.create({
     subredditPreview: {
-        margin: 10,
+        margin: 9,
         backgroundColor: 'white',
+        borderColor: theme.COLORS.LINE
     }
 })
 

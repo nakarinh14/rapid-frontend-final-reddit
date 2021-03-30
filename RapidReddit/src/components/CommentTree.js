4af9fa4fcf1a173d30be580e48bd5a5cd57569ac
nakarinh14 <nakarinh14@gmail.com>
2021-03-30 22:33:23 +0700

    Improve Comment Hide

--- RapidReddit/src/components/CommentTree.js
+++ RapidReddit/src/components/CommentTree.js
@@ -7,20 +7,21 @@ export const CommentTree = ({ comment, depth, path='', visibility }) => {
     const newDepth = depth+1
     const [hide, setHide] = useState(false)
 
+    const displayCss = visibility  ? 'none' : undefined
     if(comment){
         const childrenComment = comment.comments;
         return (
             <>
                 <TouchableOpacity
                     onPress={() => setHide(!hide)}
-                    style={{flexDirection: "row", alignItems: 'center', justifyContent: 'flex-end', display: undefined}}
+                    style={{flexDirection: "row", alignItems: 'center', justifyContent: 'flex-end', display: displayCss}}
                 >
                     <Comment comment={{...comment.comment, id: comment.id}} depth={depth} path={path} hide={hide}/>
                 </TouchableOpacity>
                 {
-                    !hide && childrenComment && Object.keys(childrenComment).map((child_id) =>
+                    childrenComment && Object.keys(childrenComment).map((child_id) =>
                         <CommentTree
-                            visibility={hide}
+                            visibility={visibility || hide}
                             key={`child-${depth}-${child_id}`}
                             comment={childrenComment[child_id]}
                             depth={newDepth}
